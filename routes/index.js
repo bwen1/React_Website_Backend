411081d7f9d64ac6a6067ed9d96a379253eb7596
bwen1 <bwen1@live.com>
2019-05-28 00:11:18 +1000

    done search route, still need filters

--- routes/index.js
+++ routes/index.js
@@ -26,11 +26,11 @@ router.get('/api/genders', (req, res) => {
         let gender = result.map((val, i) => {
             return val.gender;
         });
+        console.log(gender);
+        res.status(200)
+            .send(gender)
+            .end();
     });
-    console.log(gender);
-    res.status(200)
-        .send(gender)
-        .end();
 });
 
 router.get('/api/area', (req, res) => {
@@ -44,11 +44,11 @@ router.get('/api/area', (req, res) => {
         let area = result.map((val, i) => {
             return val.area;
         });
+        console.log(area);
+        res.status(200)
+            .send(area)
+            .end();
     });
-    console.log(area);
-    res.status(200)
-        .send(area)
-        .end();
 });
 
 router.get('/api/ages', (req, res) => {
@@ -62,11 +62,11 @@ router.get('/api/ages', (req, res) => {
         let age = result.map((val, i) => {
             return val.age;
         });
+        console.log(age);
+        res.status(200)
+            .send(age)
+            .end();
     });
-    console.log(age);
-    res.status(200)
-        .send(age)
-        .end();
 });
 
 router.get('/api/years', (req, res) => {
@@ -80,11 +80,12 @@ router.get('/api/years', (req, res) => {
         let years = result.map((val, i) => {
             return val.years;
         });
+
+        console.log(years);
+        res.status(200)
+            .send(years)
+            .end();
     });
-    console.log(years);
-    res.status(200)
-        .send(years)
-        .end();
 });
 
 router.get('/api/offences', (req, res) => {
@@ -107,6 +108,59 @@ router.get('/api/offences', (req, res) => {
         console.log(pretty);
         res.status(200)
             .send(pretty)
+            .end();
+    });
+});
+
+router.get('/api/search', (req, res) => {
+    let term = req.query.term;
+    //term = 'arson';
+    let query = `SELECT area, ${term} FROM offences`;
+    console.log(query);
+
+    //let query = `SELECT * FROM world.city WHERE name = "${term}"`;
+
+    req.app.locals.db.query(query, (err, result) => {
+        if (err) {
+            console.log(err, result);
+            res.redirect('/');
+        }
+        console.log(result);
+        // TODO: Implement filters
+        let search = result.reduce((acc, cur) => {
+            acc[cur.area] = acc[cur.area] || 0 + cur[term];
+            return acc;
+        }, {});
+
+        console.log(search);
+        res.status(200)
+            .send(search)
+            .end();
+    });
+});
+
+router.get('/api/searchtest', (req, res) => {
+    // const { term } = req.query; // Better syntax
+    //const term = req.query.term;
+    //res.send(['brisbane', 'sydney']);
+    //const query = `SELECT * FROM world.city WHERE name = "${term}"`;
+    let query = `SELECT area,arson FROM offences`;
+    //var table = ['city'];
+
+    req.app.locals.db.query(query, (err, result) => {
+        if (err) {
+            console.log(err, result);
+            res.redirect('/');
+        }
+        //console.log(result);
+        let search = result.reduce((acc, cur) => {
+            acc[cur.area] = acc[cur.area] || 0 + cur.arson;
+            return acc;
+        }, {});
+
+        console.log(search);
+        res.status(200)
+            .send(search)
             .end();
     });
 });
